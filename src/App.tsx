43a8bba6d8c0c0d986ccbe9ff1b5246519d48355
Camilo <osoriojuancamilo315@gmail.com>
2025-11-04 21:32:01 -0500

    FEAT: Agrega el AOS en la App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import AppRoutes from "./routes/AppRoutes";
+import "aos/dist/aos.css";
+import AOS from "aos";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1500,
+      once: false,
+      mirror: true,
+      offset: 120
+    });
+  }, []);
 
   return (
     <>
