07a2e1f19f64e74d9a0257b4c02ca6e31d898d12
Camilo <osoriojuancamilo315@gmail.com>
2025-11-04 21:32:40 -0500

    CHORE: Agrega las rutas

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,8 @@
+import Page404 from "@/pages/404";
+import Contacto from "@/pages/Contacto";
 import Index from "@/pages/Index";
+import QuienesSomos from "@/pages/QuienesSomos";
+import Services from "@/pages/Services";
 import { Route, Routes } from "react-router";
 
 export default function AppRoutes() {
@@ -6,6 +10,10 @@ export default function AppRoutes() {
         <>
             <Routes>
                 <Route path="/" element={<Index />} />
+                <Route path="/quienes-somos" element={<QuienesSomos />} />
+                <Route path="/contacto" element={<Contacto />} />
+                <Route path="/servicios/:servicio" element={<Services />} />
+                <Route path="*" element={<Page404 />} />
             </Routes>
         </>
     );
