d70db21c3a00b8268a9e9510877244b62e5a81f9
Camilo <osoriojuancamilo315@gmail.com>
2025-11-04 21:24:35 -0500

    FEAT: Agrega el scroll top al cambio de pagina

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { createRoot } from 'react-dom/client';
 import './style/index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router';
+import ScrollToTop from './hooks/scrollToTop.ts';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>,
 );
